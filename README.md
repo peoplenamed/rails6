76a328294a9f0b507c2899889f5e9fd06b57b186
Pete <peoplenamed@gmail.com>
2021-05-28 14:08:42 -0400

    Updated to Rails 6! Ummm. I thought there was more here. Users seem to work. No real links for anything. Just a silly colorful BG.

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # README
+	RUBY: 3.0.0
+	RAILS:
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
 
-Things you may want to cover:
+	# INSTALL PREREQUISITES
+		sudo apt install curl g++ gcc autoconf automake bison libc6-dev \
+        libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool \
+        libyaml-dev make pkg-config sqlite3 zlib1g-dev libgmp-dev \
+        libreadline-dev libssl-dev
 
-* Ruby version
 
-* System dependencies
+    # INSTALL RUBY VERSION
+    	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-* Configuration
+    	## READ THE OUTPUT AND FOLLOW THE INSTRUCTIONS!
+    	curl -sSL https://get.rvm.io | bash -s stable
+    	## READ THE OUTPUT AND FOLLOW THE INSTRUCTIONS!
+    		### HINT: source /home/pete/.rvm/scripts/rvm
 
-* Database creation
+    	rvm list known
 
-* Database initialization
+    	rvm install ruby
 
-* How to run the test suite
+    	rvm --default use ruby
 
-* Services (job queues, cache servers, search engines, etc.)
+    	# cd in to app directory and install bundler.
+    	gem install bundler
 
-* Deployment instructions
-
-* ...
+    	bundle install
--- db/schema.rb
+++ db/schema.rb
@@ -2,54 +2,54 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305092204) do
+ActiveRecord::Schema.define(version: 2017_03_05_092204) do
 
   create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.date     "dob"
-    t.integer  "user_id",    limit: 10
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.integer "user_id", limit: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "phone_numbers", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "user_id",      limit: 10
-    t.integer  "phone_number", limit: 10
-    t.string   "carrier"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.integer "person_id"
+    t.integer "user_id", limit: 10
+    t.integer "phone_number", limit: 10
+    t.string "carrier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
